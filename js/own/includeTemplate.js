989ead4aafd320fe1c4c6d855013a64fcc956739
ivanof <ivaylo.ivanof@gmail.com>
2016-03-12 08:45:52 +0200

    creating template with using mustache

---
+++ js/own/includeTemplate.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Ivaylo Ivanov on 16-3-12.
+ */
+
+
+//include navigation on site
+var navigation = (function() {
+	$.get('html/navigation.html', function(template) {
+		var rendered = Mustache.render(template);
+		$('header').html(rendered);
+	})
+})();
+
+//include post on site
+var post = (function() {
+	$.get('html/post.html', function (template) {
+		var rendered = Mustache.render(template);
+		$('.content').html(rendered);
+	})
+})();
