5bd768f8aa3bf8fb1bb70c4b018c161e46bfdf0a
ivanof <ivaylo.ivanof@gmail.com>
2016-03-12 13:33:33 +0200

    config router

--- js/own/route.js
+++ js/own/route.js
@@ -1,9 +1,31 @@
 /**
  * Created by Ivaylo Ivanov on 16-3-12.
  */
+var titinessisBlog = titinessisBlog || {};
 
-//Sammy.get('/post', function() {
-//	alert('safsg');
-//});
+(function() {
+	titinessisBlog.router = Sammy(function() {
+		$('#testing').text('Welcome!');
 
-console.log(Sammy);
+		this.get('#/home', function() {
+			$("#testing").text('Home');
+			home();
+		});
+
+		this.get('#/post', function() {
+			$("#testing").text('Post');
+			post();
+		});
+
+		this.get('#/addpost', function() {
+			$("#testing").text('Post');
+			post();
+		});
+
+		this.get('#/login', function() {
+			this.redirect('#/register');
+		});
+	});
+
+	titinessisBlog.router.run('#/home');
+})();
