2da9dc2ad71cd084a6e93f946b63a13607a8055d
ivanof <ivaylo.ivanof@gmail.com>
2016-03-12 15:51:44 +0200

    update

--- js/own/kinveyQuery.js
+++ js/own/kinveyQuery.js
@@ -2,3 +2,9 @@
  * Created by Ivaylo Ivanov on 16-3-12.
  */
 
+var promise = Kinvey.init({
+	appKey    : 'kid_b1HhUaVLJb',
+	appSecret : '916fc5a992ee453780b58f565e2b50ad'
+}).then(function() {
+	console.log(promise);
+});
