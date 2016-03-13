ce277dad7a80e0b691e5164d3bd3b48d92d03fc5
ivanof <ivaylo.ivanof@gmail.com>
2016-03-13 19:09:38 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -3,48 +3,41 @@
  */
 var app = app || {};
 
-if (sessionStorage.length === 0) {
-	sessionStorage.setItem('user', 'guest');
-	sessionStorage.setItem('permission', 'guest');
-}
-
 (function() {
 	app.router = Sammy(function() {
 		app.requester.config('kid_b1HhUaVLJb', '916fc5a992ee453780b58f565e2b50ad');
 		var userRequester = new app.userRequester();
-		//userRequester.signUp("ivan", 'ivanov', 'ivan@abv.bg');
-		userRequester.login('ivan', 'ivanov');
-		userRequester.getInfo();
-		console.log(userRequester);
-
-		$('#testing').text('Welcome!');
 
 		this.get('#/home', function() {
-			$("#testing").text('Home');
 			home();
 		});
 
 		this.get('#/post', function() {
-			$("#testing").text('Post');
 			post();
 		});
 
 		this.get('#/addpost', function() {
 			//if not entry in blog
-			if (sessionStorage.getItem('user') === 'guest' &&
-				sessionStorage.getItem('permission') === 'guest') {
+			if (sessionStorage.length === 0) {
 				this.redirect('#/login');
 			} else {
-				$("#testing").text('AddPost');
 				addPost();
 			}
 		});
 
 		this.get('#/login', function() {
 			//this.redirect('#/register');
-			$("#testing").text('Login');
+			//userRequester.login('ivan', 'ivanov');
+			//$('#loginForm').submit(function() {
+			//	console.log(this);
+			//});
 			login();
 		});
+
+		this.get('#/register', function() {
+			//userRequester.signUp("ivaylo", 'ivanov', 'ivaylo@abv.bg');
+			register();
+		})
 	});
 
 	app.router.run('#/home');
