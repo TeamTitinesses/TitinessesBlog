ddd4af8a220ac789d1cee955b2389956b6e2b2b3
ivanof <ivaylo.ivanof@gmail.com>
2016-03-13 21:08:22 +0200

    add postRequester

--- js/app.js
+++ js/app.js
@@ -7,8 +7,10 @@ var app = app || {};
 	app.router = Sammy(function() {
 		app.requester.config('kid_b1HhUaVLJb', '916fc5a992ee453780b58f565e2b50ad');
 		var userRequester = new app.userRequester();
+		var postRequester = new app.postRequester();
 
 		this.get('#/home', function() {
+			postRequester.getAllPost();
 			home();
 		});
 
@@ -28,9 +30,9 @@ var app = app || {};
 		this.get('#/login', function() {
 			//this.redirect('#/register');
 			userRequester.login('ivaylo', 'ivanov');
-			//$('#loginForm').submit(function() {
-			//	console.log(this);
-			//});
+			$('#loginForm').submit(function() {
+				console.log(this);
+			});
 			login();
 		});
 
---
+++ js/models/postRequester.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Ivaylo Ivanov on 16-3-13.
+ */
+
+var app = app || {};
+
+app.postRequester = (function () {
+	function PostRequester() {
+		this.serviceUrl = app.requester.baseUrl + 'appdata/' + app.requester.appId;
+	}
+
+	//get all post
+	PostRequester.prototype.getAllPost = function () {
+		var defer = Q.defer();
+		var requestUrl = this.serviceUrl + '/post';
+
+		app.requester.makeRequest('GET', requestUrl, null, true)
+			.then(function (success) {
+				console.log(success);
+				defer.resolve();
+			}, function (error) {
+				console.error(error);
+				defer.reject();
+			}).done();
+
+		return defer.promise;
+	};
+
+	return PostRequester;
+
+})();
