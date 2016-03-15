3d9fb245277b51f1fe8d5da8d6f2d1ba8ef1ebf2
ivanof <ivaylo.ivanof@gmail.com>
2016-03-15 22:29:08 +0200

    show post on post page

--- js/view/postViews.js
+++ js/view/postViews.js
@@ -2,15 +2,10 @@ var app = app || {};
 
 app.postViews = (function () {
     function showPosts(selector, posts) {
-        $.get('templates/home.html', function (template) {
+        $.get('templates/posts.html', function (template) {
 			var viewData = {
 				data: posts
 			};
-			//posts.forEach(function(el) {
-			//	if(typeof el.content === 'object') {
-			//		console.log(el);
-			//	}
-			//});
 
             var rendered = Mustache.render(template, viewData);
             $(selector).html(rendered);
